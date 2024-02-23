d3ee3e0b6dce7adeff3c467c65d86e1b5503280d
Thomas Azran <tommy.azran@gmail.com>
2024-02-23 19:08:23 +0000

    add helpful comments

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,25 +1,35 @@
 const request = require("request");
 
-const commandLineContent = process.argv[2] ? process.argv[2].toLowerCase() : undefined;
+const commandLineContent = process.argv[2]
+  ? process.argv[2].toLowerCase()
+  : undefined;
 
-const fetchBreedDescription = function(breedName, callback) {
+// Declares a function named fetchBreedDescription which takes two parameters: breedName and callback
+const fetchBreedDescription = function (breedName, callback) {
+  // Checks if breedName is truthy, if so, converts it to lowercase; otherwise, sets it to undefined
   breedName = breedName ? breedName.toLowerCase() : undefined;
-  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
-    if (error) {
-      callback("Error:", error);
-      return;
-    }
-    const breeds = JSON.parse(body);
-    const matchBreed = breeds.find(breed => breed.name.toLowerCase().includes(breedName));
-  
-    if (matchBreed) {
-      callback(null, matchBreed.description);
-    } else {
-      callback(null, `Breed not found`);
-    }
-  });
+  // Makes a request to the specified URL using the request function
+  // The URL contains the breedName parameter interpolated into the string
+  request(
+    `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`,
+    (error, response, body) => {
+      if (error) {
+        callback("Error:", error);
+        return;
+      }
+      // Parses the response body from JSON format to a JavaScript object
+      const breeds = JSON.parse(body);
+      // Searches for a breed within the breeds array that matches the provided breedName
+      // The matchBreed variable will store the matched breed object
+      const matchBreed = breeds.find((breed) => breed.name.toLowerCase().includes(breedName));
 
-
+      if (matchBreed) {
+        callback(null, matchBreed.description);
+      } else {
+        callback(null, `Breed not found`);
+      }
+    }
+  );
 };
 
-module.exports = {fetchBreedDescription};
+module.exports = { fetchBreedDescription };
--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const { fetchBreedDescription } = require('./breedFetcher');
 
+// Retrieves the command line argument provided at index 2
+// If no argument is provided, it sets commandLineContent to undefined
 const commandLineContent = process.argv[2] ? process.argv[2].toLowerCase() : undefined;
-
+// Calls the fetchBreedDescription function with the commandLineContent as the breedName parameter
+// It also provides a callback function to handle the response from fetchBreedDescription
 fetchBreedDescription(commandLineContent, (error, desc) => {
   if (error) {
     console.log('Error fetch details:', error);
